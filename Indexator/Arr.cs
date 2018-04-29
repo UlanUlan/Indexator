2bd550a3f40c5be8a992f206605413bc8f07183a
One <real_user@list.ru>
2018-04-29 18:16:03 +0600

    Add project files.

---
+++ Indexator/Arr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexator
+{
+    public class Arrr
+    {
+        int[] a;
+        private Random rand = new Random();
+        public Arrr()
+        {
+            a = new int[rand.Next(10,30)];
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return a[index];
+            }
+            set
+            {
+                a[index] = value*value;
+            }
+        }
+        
+
+
+    }
+}
---
+++ Indexator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Arrr arrr = new Arrr();
+                arrr[0] = 5;
+                Console.WriteLine(arrr[0]);  
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
